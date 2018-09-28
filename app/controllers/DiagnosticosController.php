09dc38289748c550a3a481d146bfc89abc3ad746
Cristian Rios <criosmon2345@universidadean.edu.co>
2018-09-28 09:59:30 -0500

    prueba

--- app/controllers/DiagnosticosController.php
+++ app/controllers/DiagnosticosController.php
@@ -625,27 +625,27 @@ class DiagnosticosController extends ControllerBase
          }
 
          //Obtener total de preguntas por categoria
-         $preguntas_tot = diag\cc\Pregunta::find(['id_diagnostico = ?0',
-                        'bind' => [ $intento->id_diagnostico ],]); 
-         foreach($preguntas_tot as $pr_tot){
-             if(isset($tot_cat[$pr_tot->id_categoria]){
-                $tot_cat[$pr_tot->id_categoria] ++;
-             }else{
-                $tot_cat[$pr_tot->id_categoria] = 1;
-             }      
-         }
+        //  $preguntas_tot = diag\cc\Pregunta::find(['id_diagnostico = ?0',
+        //                 'bind' => [ $intento->id_diagnostico ],]); 
+        //  foreach($preguntas_tot as $pr_tot){
+        //      if( isset( $tot_cat[$pr_tot->id_categoria] ){
+        //         $tot_cat[$pr_tot->id_categoria] ++ ;
+        //      }else{
+        //         $tot_cat[$pr_tot->id_categoria] = 1 ;
+        //      }      
+        //  }
 
-         //Armar arreglo final
-         foreach($preguntas as $pregunta){
-            $categoria = diag\cc\Categoria::findfirst($pregunta->id_categoria);
-            if(isset($resultado[$pregunta->id_categoria]){
-                $resultado[$pregunta->id_categoria] ++;
-            }else{
-                $resultado[$pregunta->id_categoria] = 1;
-            }
-            $set_resultados[$categoria->titulo] = ['total_bien'      => $resultado[$pregunta->id_categoria],
-                                                   'total_preguntas' => $tot_cat[$pregunta->id_categoria],];
-         }
+        //  //Armar arreglo final
+        //  foreach($preguntas as $pregunta){
+        //     $categoria = diag\cc\Categoria::findfirst($pregunta->id_categoria);
+        //     if( isset( $resultado[$pregunta->id_categoria] ){
+        //         $resultado[$pregunta->id_categoria] ++ ;
+        //     }else{
+        //         $resultado[$pregunta->id_categoria] = 1 ;
+        //     }
+        //     $set_resultados[$categoria->titulo] = ['total_bien'      => $resultado[$pregunta->id_categoria],
+        //                                            'total_preguntas' => $tot_cat[$pregunta->id_categoria],];
+        //  }
 
          $response->setJsonContent(
             [
